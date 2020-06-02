ffcd8e5646c37a8c1a024a4b0de5d88e21a4a1a4
manimaran_vellaichamy@epam.com <Manimaran_Vellaichamy@epam.com>
2020-06-02 10:14:45 +0530

    with jest test cases

--- e4.js
+++ e4.js
@@ -1,6 +1,3 @@
-var a = [[1, 2], [3, 4]];
-var b = [[4, 3], [2, 1]];
-var c = [[1, 3], [2, 4]];
 //a and b are equal while a and c are not equal
 function compareArray(a1, a2) {
     //length check
@@ -33,41 +30,5 @@ function compareArray(a1, a2) {
    console.log(a1, a2, `Proper match`);
     return true;
 }
-if(! compareArray(a, c) ){ 
-    console.log("not proper match");
-}
-compareArray(a, b);
-compareArray(b, c);
 
-c1 = [
-    [3,2,2],
-    [7,4,2]
-]
-
-c2 = [
-    [2,3,2],
-    [7,4,2]
-]
-
-c3 = [
-    [3,1,2],
-    [2,7,2]
-]
-
-c4 = [
-    [1,2],
-    [2,3,4,5],
-    [6,7,8]
-]
-
-c5 = [
-    [7,6,8],
-    [5,3,4,2],
-    [2,1]
-]
-console.log(
-    c1,c2, compareArray(c1, c2), true,  "\n",
-    c1,c3, compareArray(c1, c3), false, "\n",
-    c3,c2, compareArray(c3, c2), false, "\n",
-    c4,c5, compareArray(c4, c5), true, "\n",
-)
+module.exports = compareArray;
